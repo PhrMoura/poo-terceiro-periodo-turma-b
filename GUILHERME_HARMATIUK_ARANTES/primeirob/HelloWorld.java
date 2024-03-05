83d671c2b10ceb888b6f003738aa69e31b4414d7
Gharantes <work.gharantes@gmail.com>
2024-03-05 20:46:59 -0300

    msg commit

---
+++ GUILHERME_HARMATIUK_ARANTES/primeirob/HelloWorld.java
@@ -0,0 +1,8 @@
+package primeirob;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Hello there!");
+    }
+}
