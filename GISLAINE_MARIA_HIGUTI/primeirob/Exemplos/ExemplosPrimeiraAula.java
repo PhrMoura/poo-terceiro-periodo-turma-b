784e3c2174ea3b25314b9c1b79da04d4c223823e
gutigangm <gutigangm@gmail.com>
2024-03-05 22:05:24 -0300

    HelloWorld

---
+++ GISLAINE_MARIA_HIGUTI/primeirob/Exemplos/ExemplosPrimeiraAula.java
@@ -0,0 +1,46 @@
+package primeirob.Exemplos;
+
+public class ExemplosPrimeiraAula {
+    public static void main(String[] args) {
+
+        System.out.println("Inteiros:");
+
+        byte parcela = 23;
+        System.out.println("byte: " + parcela); // byte
+
+        short idade = 12;
+        System.out.println("short: " + idade); //short
+
+        System.out.println("----------------");
+
+        int cep = 85806137;
+        System.out.println("int: " + cep); //int
+
+        System.out.println("----------------");
+
+        long numeroCelular = 45999887766l; //precisa ter o l no final
+        System.out.println("long: " + numeroCelular); //long
+
+        System.out.println("----------------");
+
+        System.out.println("Flutuantes, Booleanos e Caracteres");
+
+        float valorProduto = 20.5f;
+        double valorTaxa = 0.3;
+
+        System.out.println("float: " + valorProduto);
+        System.out.println("double: " + valorTaxa);
+
+        System.out.println("----------------");
+
+        char letraA = 'A';
+        System.out.println("char: " + letraA);
+
+        System.out.println("----------------");
+
+        boolean isMaiorDeIdade = false;
+        System.out.println("booleano: " + isMaiorDeIdade);
+
+        System.out.println("----------------");
+    }
+}
