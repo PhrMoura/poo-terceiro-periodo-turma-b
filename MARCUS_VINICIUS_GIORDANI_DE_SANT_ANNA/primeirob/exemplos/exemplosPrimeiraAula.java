bee273c92a01096fddea8799daa34f2dc950c95d
MarcusGiordani <marcus.giordani15@gmail.com>
2024-03-05 21:57:51 -0300

    add hello world

---
+++ MARCUS_VINICIUS_GIORDANI_DE_SANT_ANNA/primeirob/exemplos/exemplosPrimeiraAula.java
@@ -0,0 +1,28 @@
+package primeirob.exemplos;
+
+public class exemplosPrimeiraAula {
+    public static void main(String[] args) {
+
+        byte parcela = 12;
+        System.out.println("byte: " + parcela);
+        //byte
+
+        short idade = 18;
+        System.out.println("short: " + idade);
+        //short
+
+       int cep = 86806066;
+       System.out.println("int: " + cep);
+        //int
+
+        long numeroCelular = 46999162706l;
+        System.out.println("long: " + numeroCelular);
+        //long
+
+        float valorProduto = 20.5f;
+        double valorTaxa = 0.3;
+        System.out.println("float: " + valorProduto);
+        System.out.println("double: " + valorTaxa);
+    }
+    
+}
