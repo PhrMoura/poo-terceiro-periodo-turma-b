bad2d4b71e88a1b00e65aeb77f30f7f4653dddd5
Lari7l <larissa.rfrasseto@gmail.com>
2024-03-12 15:42:47 -0300

    Lista01

---
+++ LARISSA_RODRIGUES/primeirob/Exemplos/HelloWorld.java
@@ -0,0 +1,7 @@
+package primeirob;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World"); // usa "sout" pra escrever o "system ..."
+    }
+}
--- LARISSA_RODRIGUES/primeirob/HelloWorld.java
+++
@@ -1,7 +0,0 @@
-package primeirob;
-
-public class HelloWord {
-    public static void main(String[] args) {
-        System.out.println("Hello Word"); // usa "sout" pra escrever o "system ..."
-    }
-}
