97ca5fb30e6c2e893d32bf4d02cb4a04ff677675
Michelliberali <michelliberali233@gmail.com>
2024-03-12 21:25:35 -0300

    entrega lista 1

--- MICHEL_LIBERALI_SANTOS/primeirob/HelloWord.java
+++ MICHEL_LIBERALI_SANTOS/primeirob/HelloWord.java
@@ -2,7 +2,7 @@ package primeirob;
 
 public class HelloWord {
     public static void main(String[] args) {
-        System.out.println("HelloWord");
+        System.out.println("HelloWord HellowWord");
     }
 }
  
