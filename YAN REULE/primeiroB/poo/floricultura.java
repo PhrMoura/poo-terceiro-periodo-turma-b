ac7a6892aa1a647f9d88356fc5dae5c476d5fca3
Yan-Reule <yancreule@gmail.com>
2024-03-17 17:21:50 -0300

    seila

--- YAN REULE/primeiroB/poo/floricultura.java
+++ YAN REULE/primeiroB/poo/floricultura.java
@@ -33,23 +33,37 @@ public class floricultura {
     public static void calcularPrecoTotal(Scanner scanner) {
         System.out.print("Digite a quantidade da planta vendida: ");
         int quantidade = scanner.nextInt();
-        System.out.print("Digite o preço unitário da planta: ");
-        double precoUnitario = scanner.nextDouble();
 
-        double precoTotal = quantidade * precoUnitario;
+        if (quantidade > 10) {
+            System.out.print("Parabéns, compras acima de 10 quantidades ganham 5% de desconto!!");
 
-        System.out.println("O preço total da venda é: " + precoTotal);
+            System.out.print("Digite o preço unitário da planta: ");
+            double precoUnitario = scanner.nextDouble();
+
+            double precoTotal = quantidade * precoUnitario;
+            double desconto = 0.05;
+            double valorComDesconto = precoTotal * desconto;
+
+            System.out.println("O preço total com desconto é: " + valorComDesconto);
+        } else if (quantidade < 10) {
+            System.out.print("Digite o preço unitário da planta: ");
+            double precoUnitario = scanner.nextDouble();
+
+            double precoTotal = quantidade * precoUnitario;
+
+            System.out.println("O preço total da venda é: " + precoTotal);
+        }
     }
 
     public static void calcularTroco(Scanner scanner) {
         System.out.print("Digite o valor recebido pelo cliente: ");
         double valorRecebido = scanner.nextDouble();
-        
+
         System.out.print("Digite o valor total da compra: ");
         double valorTotalCompra = scanner.nextDouble();
-        
+
         double troco = valorRecebido - valorTotalCompra;
-        
+
         if (troco >= 0) {
             System.out.println("O troco a ser dado ao cliente é: " + troco);
         } else {
