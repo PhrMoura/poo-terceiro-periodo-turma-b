ba61e3057d7b351f692b22609c2c813feacb3207
Prept7K <felipeemanuelnegri@gmail.com>
2024-03-25 21:33:15 -0300

    Atividade tres pog

--- FELIPE_EMANUEL_NEGRI/primeirob/atividades/AtividadeVendinha.java
+++ FELIPE_EMANUEL_NEGRI/primeirob/atividades/AtividadeVendinha.java
@@ -1,8 +1,8 @@
-package primeirob.atividadeum;
+package primeirob.atividades;
 
 import java.util.Scanner;
 
-public class AtividadeUm {
+public class AtividadeVendinha {
     public static void main(String[] args) {
         System.out.println("Bem vindo a lojinha da Dona Gabrielinha");
         Scanner input = new Scanner(System.in);
@@ -12,10 +12,12 @@ public class AtividadeUm {
         double Recebido = 0;
         double subtotal = 0;
         double troco = 0;
+        int dia = 0, mes = 0;
 
         int[] qntPlantas = new int[99];
         double[] vlrVendas = new double[99];
         double[] vlrDesconto = new double[99];
+        double[][] diaMes = new double[30][12];
 
         int i = 0;
         while (opcao != 3) {
@@ -24,6 +26,10 @@ public class AtividadeUm {
 
             switch (opcao) {
                 case 1:
+                    System.out.println("Qual o dia e mês da venda? ");
+                    dia = input.nextInt();
+                    mes = input.nextInt();
+
                     System.out.println("Quantas plantas você vai querer?");
                     Plantas = input.nextInt();
                     qntPlantas[i] = Plantas;
@@ -42,6 +48,8 @@ public class AtividadeUm {
                     vlrDesconto[i] = desconto;
                     i++;
                     System.out.println("Valor total: " + subtotal);
+
+                    diaMes[dia - 1][mes - 1] += subtotal;
                     break;
                 case 2:
                     System.out.println("Quantos reais foram recebidos?");
@@ -57,18 +65,24 @@ public class AtividadeUm {
                     System.out.println("Volte sempre :)");
                     System.out.println("Registro ");
                     break;
-                case 5:
+                case 4:
                     for (int c = 0; c <= 99; c++) {
                         if (qntPlantas[c] == 0) {
                             break;
                         } else {
-                            System.out.println("----------------------");
                             System.out.println("Quantidade de plantas: " + qntPlantas[c]);
                             System.out.println("O valor da venda é: " + vlrVendas[c]);
                             System.out.println("O desconto aplicado é: " +vlrDesconto[c]);
                             System.out.println("----------------------");
                         }
                     }
+                    break;
+                case 5: 
+                    System.out.println("Qual dia deseja verificar o total vendido? ");
+                    dia = input.nextInt();
+                    System.out.println("De qual mes? ");
+                    mes = input.nextInt();
+                    System.out.println("Dia " + dia + " Mês " + mes + " Valor total é " + diaMes[dia - 1][mes - 1]);
                     break;
                 default:
                     System.out.println("Opção inválida! Tente novamente.");
@@ -84,7 +98,8 @@ public class AtividadeUm {
         System.out.println("|OPÇÃO [1] Comprar plantinhas");
         System.out.println("|OPÇÃO [2] Calcular o Troco");
         System.out.println("|OPÇÃO [3] Sair");
-        System.out.println("|OPCÃO [5] quantas plantas foi vendida???");
+        System.out.println("|OPCÃO [4] Quantas plantas foi vendida?");
+        System.out.println("|OPCÃO [5] Em que dia foi feito a compra?");
     }
 
 }
