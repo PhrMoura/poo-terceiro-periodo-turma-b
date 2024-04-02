fd5aad67a6bf355153872eaf40d6d32cdf01912c
Brunocallegaro <brunoeduardocallegaro@gmail.com>
2024-04-02 20:18:15 -0300

    Exemplo aula 6

---
+++ BRUNO_EDUARDO_CALLEGARO/Aula6/Professor.java
@@ -0,0 +1,40 @@
+package Aula6;
+
+public class Professor {
+        String nome; 
+
+        String materia; 
+    
+        public void apresentarse() { 
+            String msg = "Olá! Sou o professor(a):"
+            .concat(nome)
+            .concat(" e vou lecionar ")
+            .concat(materia);
+    
+            System.out.println(msg);
+    
+            Professor profeUm = new Professor(); 
+            Professor oMelhorProfe = new Professor();
+            
+            profeUm.nome = "Roberto"; 
+            profeUm.materia = "Projeto de Software"; 
+    
+            oMelhorProfe.nome = "Sandro"; 
+            oMelhorProfe.materia = "POO"; 
+    
+            oMelhorProfe.apresentarse();
+    
+            
+        }
+           
+        
+          
+    
+        
+        
+    }
+   
+
+
+   
+
