6e78d6e38d120b833db2aaa01e7f570b4ceb1a1e
Brunocallegaro <brunoeduardocallegaro@gmail.com>
2024-04-16 19:50:28 -0300

    Entrega atividade 6

---
+++ ALISON_ANTHOY_GALVAN/Atv6/Gerente.java
@@ -0,0 +1,41 @@
+package a;
+
+public class Gerente {
+    String nome;
+    int idade;
+    String loja;
+    String cidade;
+    String bairro;
+    String rua;
+    double salarioBase;
+    double[] salarioRecebido;
+
+    public Gerente(String nome, int idade, String loja, String cidade, String bairro, String rua, double salarioBase, double[] salarioRecebido) {
+        this.nome = nome;
+        this.idade = idade;
+        this.loja = loja;
+        this.cidade = cidade;
+        this.bairro = bairro;
+        this.rua = rua;
+        this.salarioBase = salarioBase;
+        this.salarioRecebido = salarioRecebido;
+    }
+
+    public void apresentarse() {
+        System.out.println("Nome: " + nome);
+        System.out.println("Idade: " + idade);
+        System.out.println("Loja: " + loja);
+    }
+
+    public double calcularMedia() {
+        double soma = 0;
+        for (double salario : salarioRecebido) {
+            soma += salario;
+        }
+        return soma / salarioRecebido.length;
+    }
+
+    public double calcularBonus() {
+        return salarioBase * 0.35;
+    }
+}
