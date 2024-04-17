1e4d80a8d8ba196768f5368362bb9a02a5d11f6b
vivihzg <vivihzgithub@gmail.com>
2024-04-16 21:24:51 -0300

    Lista 6

---
+++ MARIA_VITORIA_DA_SILVA/Lista5/Main.java
@@ -0,0 +1,42 @@
+package Lista5;
+
+import Lista5.Informacoes.Cliente;
+import Lista5.Informacoes.Vendedor;
+
+public class Main {
+    public static void main(String[] args) {
+        Vendedor primeiroVendedor = new Vendedor();
+        Cliente primeiroCliente = new Cliente();
+        Informacoes testInstance = new Informacoes(); 
+        Informacoes.Loja primeiraLoja = testInstance.new Loja(); 
+
+        primeiraLoja.setVendedores(new Vendedor[]{primeiroVendedor});
+        primeiraLoja.setClientes(new Cliente[]{primeiroCliente});
+
+        primeiroVendedor.nomeVendedor = "Robson";
+        primeiroVendedor.idadeVendedor = 19;
+        primeiroVendedor.lojaVendedor = "Myy Plant";
+        primeiroVendedor.cidadeVendedor = "Cascavel";
+        primeiroVendedor.bairroVendedor = "Centro";
+        primeiroVendedor.ruaVendedor = "Paraná 1";
+
+        primeiroVendedor.apresentarse();
+
+        primeiroCliente.nomeCliente = "Vitoria";
+        primeiroCliente.idadeCliente = 30;
+        primeiroCliente.cidadeCliente = "Cascavel";
+        primeiroCliente.bairroCliente = "São Cristóvão";
+        primeiroCliente.ruaCliente = "Paraná 6";
+
+        primeiroCliente.apresentarse();
+
+        primeiraLoja.nomeFantasia = "Myy Plant";
+        primeiraLoja.razaoSocial = "Myy Plant Ltda";
+        primeiraLoja.cnpj = 123456789;
+        primeiraLoja.cidade = "Cascavel";
+        primeiraLoja.bairro = "Centro";
+        primeiraLoja.rua = "Paraná 1";
+
+        primeiraLoja.apresentarse();
+    }
+}
