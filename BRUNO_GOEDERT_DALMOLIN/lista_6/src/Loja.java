1639ca837c9108b6e7981b72ca5c13bf49515b64
GoedertDalmolin <gbrunodalmolin@gmail.com>
2024-04-16 21:52:14 -0300

    Arquivos Lista 6.

---
+++ BRUNO_GOEDERT_DALMOLIN/lista_6/src/Loja.java
@@ -0,0 +1,38 @@
+import java.util.List;
+
+public class Loja {
+    String nomefantasia;
+    String razaoSocial;
+    String cpnj;
+    String cidade;
+    String bairro;
+    String rua;
+    List<Vendedor> vendedores;
+    List<Cliente> clientes;
+
+    public Loja(String nomefantasia, String razaoSocial, String cpnj, String cidade, String bairro, String rua,
+            List<Vendedor> vendedores, List<Cliente> clientes) {
+        this.nomefantasia = nomefantasia;
+        this.razaoSocial = razaoSocial;
+        this.cpnj = cpnj;
+        this.cidade = cidade;
+        this.bairro = bairro;
+        this.rua = rua;
+        this.vendedores = vendedores;
+        this.clientes = clientes;
+    }
+
+    public int contarClientes() {
+        return clientes.size();
+    }
+
+    public int contarVendedores() {
+        return vendedores.size();
+    }
+
+    public String toString() {
+        return "Nome Fantasia: " + nomefantasia + ". CNPJ: " + cpnj + ". Cidade: " + cidade + ". Bairro: " + bairro
+                + ". Rua:" + rua;
+    }
+
+}
