8b1d73e69e2977542345c1cf757481844ef18c3e
vivihzg <vivihzgithub@gmail.com>
2024-04-23 12:40:07 -0300

    Lista 7

--- MARIA_VITORIA_DA_SILVA/Lista6/Item.java
+++ MARIA_VITORIA_DA_SILVA/Lista6/Item.java
@@ -1,10 +1,10 @@
 package Lista6;
 
 public class Item {
-    int id;
-    String nome;
-    String tipo;
-    double valor;
+    private int id;
+    private String nome;
+    private String tipo;
+    private double valor;
 
     public Item(int id, String nome, String tipo, double valor) {
         this.id = id;
@@ -13,14 +13,23 @@ public class Item {
         this.valor = valor;
     }
 
-    public void gerarDescricao() {
-        System.out.println("===========================");
-        System.out.println("Item: \nId do pedido: " + id + "\nNome do pedido: " + nome + "\nTipo: " + tipo + "\nValor do pedido: R$" + valor);
-        System.out.println("===========================");
+    public int getId() {
+        return id;
+    }
+    public String getNome(){
+        return nome;
+    }
+    public String getTipo(){
+        return tipo;
+    }
+    public double getValor(){
+        return valor;
     }
 
-    public double getValor() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getValor'");
+    public void gerarDescricao() {
+        System.out.println("===========================");
+        System.out.println("Item: \nId do pedido: " + id + "\nNome do pedido: " + nome + "\nTipo: " + tipo
+                + "\nValor do pedido: R$" + valor);
+        System.out.println("===========================");
     }
 }
