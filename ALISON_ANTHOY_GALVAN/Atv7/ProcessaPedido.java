3f9ddda4fefd60474356e368a3c045526555f5e9
Dazorafi <alison.galvan005@gmail.com>
2024-04-23 23:50:17 -0300

    Entrega atividade 7

---
+++ ALISON_ANTHOY_GALVAN/Atv7/ProcessaPedido.java
@@ -0,0 +1,35 @@
+import java.util.Date;
+import java.util.Scanner;
+
+public class ProcessaPedido {
+    private Scanner scanner;
+    private Item[] listaItens;
+
+    public ProcessaPedido() {
+
+    }
+
+    public Pedido processar(int id, Date dataCriacao, Date dataPagamento, Date dataVencimentoReserva, String cliente, String vendedor, String loja, String empresaParceira) {
+
+    }
+
+    private void confirmarPagamento(Pedido pedido) {
+
+    }
+
+    private Item escolherItem() {
+
+    }
+
+    public void cadastrarItem() {
+
+    }
+
+    public void listarItens() {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
