0fdd217fa5a78f5febe20b0806fed642f729d9eb
Sandrolaxx <sandro--ramos@hotmail.com>
2024-05-30 12:17:39 -0300

    feat: add samples

--- SANDRO_MATHEUS_RAMOS/segundob/aulas/aulacinco/JOptionSample.java
+++ SANDRO_MATHEUS_RAMOS/segundob/aulas/aulacinco/JOptionSample.java
@@ -8,13 +8,13 @@ public class JOptionSample {
 
     public static void main(String[] args) {
 
-        List<String> melhoresProfes = List.of("Rita", "Elenilds", "Andrade");
+        List<String> melhoresProfes = List.of("Rita", "Elenilds", "Andrade", "Sandrolax");
 
         String nome = JOptionPane.showInputDialog(
-                null,
+        null,
                 "Informe seu nome:",
-                "INFORME!",
-                JOptionPane.INFORMATION_MESSAGE);
+        "INFORME!",
+        JOptionPane.INFORMATION_MESSAGE);
 
         String opcaoSelecionda = (String) JOptionPane.showInputDialog(
                 null,
@@ -23,7 +23,11 @@ public class JOptionSample {
                 JOptionPane.INFORMATION_MESSAGE,
                 null,
                 melhoresProfes.toArray(),
-                melhoresProfes.toArray()[0]);
+                melhoresProfes.toArray()[1]);
+
+        if (!opcaoSelecionda.equals("Sandrolax")) {
+            throw new CustomException("Errado! Seu professor favorito não é esse!");
+        }
 
         System.out.println(nome);
         System.out.println(opcaoSelecionda);
