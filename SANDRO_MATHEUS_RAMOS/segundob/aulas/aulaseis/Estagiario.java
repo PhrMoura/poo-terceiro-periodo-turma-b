cd07d2b360c7877d2a0dcff5f565ea138948bb2a
Sandrolaxx <sandro--ramos@hotmail.com>
2024-06-04 22:26:26 -0300

    add samples

---
+++ SANDRO_MATHEUS_RAMOS/segundob/aulas/aulaseis/Estagiario.java
@@ -0,0 +1,22 @@
+package segundob.aulas.aulaseis;
+
+public class Estagiario implements IGerarFolhaPgto {
+
+    private Double salario;
+
+    public Double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(Double salario) {
+        this.salario = salario;
+    }
+
+    @Override
+    public void gerarFolhaPagamento() {
+        Double vlrFolhaEst = getSalario() + (getSalario() * 0.01) - 100d;
+
+        System.out.println("Valor folha estágiario: " + vlrFolhaEst);
+    }
+
+}
